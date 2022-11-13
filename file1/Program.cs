3f6ca8aa1958fa769c21440b2fdf2b7511ae04e7
Pavel Sawchenko <pashka.sawchenko@yandex.ru>
2022-11-13 22:07:13 +0300

    feat: file

---
+++ file1/Program.cs
@@ -0,0 +1,11 @@
+﻿int ObrSchet (int b)
+{
+    if (b == 0) return 1;
+    Console.Write("{0,4}", b);
+    return ObrSchet(b - 1);
+}
+
+Console.WriteLine("Введите натуральное число больше 1:");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a<1) Console.WriteLine("Число не больше 1");
+else ObrSchet(a);
